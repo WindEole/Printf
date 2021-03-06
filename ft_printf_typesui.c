096f145a78334165ae300835dd3119eea87e7461
Isabelle DERIGHETTI <iderighe@e2r8p15.42.fr>
2021-03-06 17:25:45 +0100

    Commit modif printf type u

--- ft_printf_typesui.c
+++ ft_printf_typesui.c
@@ -6,7 +6,7 @@
 /*   By: iderighe <iderighe@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 10:21:13 by iderighe          #+#    #+#             */
-/*   Updated: 2021/03/06 15:07:20 by iderighe         ###   ########.fr       */
+/*   Updated: 2021/03/06 17:23:47 by iderighe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,11 @@ int		ft_type_is_unint(unsigned int input, t_flags *flags)
 
 	add = 0;
 	str_input = ft_itoa_ui(input);
-	if (flags->dot == 0 && flags->width == 0 && input == 0)
-		return (0);
-/*	if (flags->dot == 0 && flags->width == 0 && input != 0)
-		return (add += ft_putstr(str_input));*/
+//	if (flags->zero != 0 && flags->dot != -1)
+//		flags->zero = 0;
+	if (input == 0 && flags->dot == 0 && flags->width == 0)
+		return (add += ft_fillwidthspace_ui(str_input, *flags) +
+				ft_putchar(' '));
 	if (flags->dot <= (int)ft_strlen(str_input) && flags->width <=
 			(int)ft_strlen(str_input))
 		return (add += ft_putstr(str_input));
@@ -36,9 +37,11 @@ int		ft_unintparse(char *str_input, t_flags *flags)
 	int		add;
 
 	add = 0;
+	if (flags->dot < 0 && flags->width != 0 && flags->zero == 1)
+		add += ft_fillwidthzero_ui(str_input, *flags) + ft_putstr(str_input);
 	if (flags->dot >= flags->width && flags->minus == 0)
 		add += ft_filldotzero(str_input, *flags) + ft_putstr(str_input);
-	if (flags->dot < flags->width && flags->minus == 0)
+	if (flags->dot < flags->width && flags->minus == 0 && flags->zero == 0)
 		add += ft_fillwidthspace_ui(str_input, *flags) +
 			ft_filldotzero(str_input, *flags) + ft_putstr(str_input);
 	if (flags->dot >= flags->width && flags->minus != 0)
@@ -70,29 +73,16 @@ int		ft_fillwidthspace_ui(char *str_input, t_flags flags)
 	return (add);
 }
 
-/*int		ft_filldotzero_ui(char *str_input, t_flags flags)
-{
-	int		add;
-
-	add = 0;
-	while (flags.dot > (int)ft_strlen(str_input))
-	{
-		add += ft_putchar('0');
-		flags.dot--;
-	}
-	return (add);
-}*/
-
-/*int		ft_fillwidthspace_ui(char *str_input, t_flags flags)
+int		ft_fillwidthzero_ui(char *str_input, t_flags flags)
 {
 	int		add;
 	
 	add = 0;
-	while (flags.dot > (int)ft_strlen(str_input))
+	while (flags.width > (int)ft_strlen(str_input))
 	{
 		add += ft_putchar('0');
-		flags.dot--;
-	}*/
+		flags.width--;
+	}
 /*	if (flags.dot >= 0)
 		while (flags.width - flags.dot > 0 && flags.width >
 				(int)ft_strlen(str_input))
@@ -106,5 +96,5 @@ int		ft_fillwidthspace_ui(char *str_input, t_flags flags)
 			add += ft_putchar(' ');
 			flags.width--;
 		}*/
-//	return (add);
-//}
+	return (add);
+}
